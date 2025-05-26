0c876ef472e9136ad2d4cdee30ace367228886ce
moppoyo07 <presingnln@gmail.com>
2025-05-26 11:33:46 +0900

    fix: correct script path in index.html for GitHub Pages

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/entry-form-app/sw.js', { scope: '/entry-form-app/' })})}
